c2937177adbc78b1a482e6e90cc3c7ac7d81c991
Marie Helene Kvello-Aune <marieheleneka@gmail.com>
2019-05-18 19:25:03 +0200

    Improved script to automatically include all files in the dotfiles directory.

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-DOTFILES=$(dirname $(realpath ${0}))
+DOTFILES="$(dirname $(realpath ${0}))/dotfiles"
+
 
 echo "Creating symlinks in home directory for relevant dotfiles in interactive mode."
 
-for dotfile in .tmux.conf .zshrc .vimrc
+for f in $(find ${DOTFILES} -maxdepth 1 -type f)
 do
-	ln -si ${DOTFILES}/dotfiles/${dotfile} ~/${dotfile}
+	dotfile="$(basename ${f})"
+	ln -si ${f} ~/${dotfile}
 done
